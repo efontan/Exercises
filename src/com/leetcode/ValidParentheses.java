2a38ebf232f7b865aa7856a50f1de84a9c49bb52
Emmanuel Fontan <efontan@despegar.com>
2018-03-27 23:19:18 -0300

    Adding ValidParentheses

---
+++ src/com/leetcode/ValidParentheses.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+Solution complexity: 
+Time: O(n)
+Size: O(n)
+*/
+
+public class ValidParentheses {
+    
+    public boolean isValid(String s) {
+        Stack<Character> openBrackets = new Stack<>();
+        
+        Map<Character,Character> validBracketsMap = new HashMap<>();
+        validBracketsMap.put('(', ')');
+        validBracketsMap.put('{', '}');
+        validBracketsMap.put('[', ']');
+                
+        for (int i=0; i<s.length(); i++) {
+            Character currentChar = s.charAt(i);
+            
+            if (validBracketsMap.keySet().contains(currentChar)) {
+                openBrackets.push(currentChar);
+            } else if (validBracketsMap.values().contains(currentChar)) {
+                if (openBrackets.isEmpty() || !validBracketsMap.get(openBrackets.pop()).equals(currentChar)) {
+                    return false;
+                }
+            }            
+        }        
+        return openBrackets.isEmpty();
+    }
+}
