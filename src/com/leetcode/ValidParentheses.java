e3a714ebde02ac9343b5e4094b9acc1d5dc1fbe8
Emmanuel Fontan <fontanemmanuel@gmail.com>
2018-04-14 19:59:00 -0300

    improve stack

--- src/com/leetcode/ValidParentheses.java
+++ src/com/leetcode/ValidParentheses.java
@@ -10,31 +10,29 @@ Time: O(n)
 Size: O(n)
 */
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
 public class ValidParentheses {
-    
+
     public boolean isValid(String s) {
-        Stack<Character> openBrackets = new Stack<>();
-        
+        Deque<Character> openBrackets = new ArrayDeque<>();
+
         Map<Character,Character> validBracketsMap = new HashMap<>();
         validBracketsMap.put('(', ')');
         validBracketsMap.put('{', '}');
         validBracketsMap.put('[', ']');
-                
+
         for (int i=0; i<s.length(); i++) {
             Character currentChar = s.charAt(i);
-            
+
             if (validBracketsMap.keySet().contains(currentChar)) {
                 openBrackets.push(currentChar);
             } else if (validBracketsMap.values().contains(currentChar)) {
                 if (openBrackets.isEmpty() || !validBracketsMap.get(openBrackets.pop()).equals(currentChar)) {
                     return false;
                 }
-            }            
-        }        
+            }
+        }
         return openBrackets.isEmpty();
     }
 }
