919df9f978c25a1736687207e07b654436251886
Emmanuel Fontan <efontan@despegar.com>
2018-04-23 16:24:53 -0300

    Adding BSTInsert

---
+++ src/com/hackerrank/BSTInsert.java
@@ -0,0 +1,37 @@
+package com.hackerrank;
+
+/**
+ * Binary Search Tree Insertion
+ * 
+ * Problem: https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem
+ * 
+ * You are given a pointer to the root of a binary search tree and a value to be inserted into the tree. 
+ * Insert this value into its appropriate position in the binary search tree and return the root of the 
+ * updated binary tree.
+ * 
+ */
+public class BSTInsert {
+
+    class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    public Node insert(Node root, int value) {
+        if (root == null) {
+            root = new Node();
+            root.data = value;
+        } else if (root.data < value) {
+            root.right = insert(root.right, value);
+        } else {
+            root.left = insert(root.left, value);
+        }
+
+        return root;
+    }
+
+
+
+}
+
